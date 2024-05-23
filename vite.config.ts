84dd9b0b937bdded33bec5db3ab3556fd5a95a8b
Boooyo <boooyo0914@gmail.com>
2024-05-23 11:06:09 +0900

    Styled-Components 업데이트 및 적용

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,23 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import react from "@vitejs/plugin-react";
+import babel from '@rollup/plugin-babel';
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix(),
+    tsconfigPaths(),
+    react({
+      babel: {
+        plugins: [
+          'babel-plugin-styled-components',
+          '@babel/plugin-transform-runtime',
+        ],
+      },
+    }),
+    babel({ babelHelpers: 'bundled' })
+  ],
 });
